58df84fb40e614a9ba19e596fbddc86418fd78df
Luc <lucmomber@outlook.com>
2025-04-14 18:11:42 +0200

    Added validation layers.

--- VulkanTest/main.cpp
+++ VulkanTest/main.cpp
@@ -6,137 +6,190 @@
 #include <cstdlib>
 #include <vector>
 #include <string>
+#include <cassert>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
-class HelloTriangleApplication 
+const std::vector<const char*> validationLayers = {
+	"VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else 
+const bool enableValidationLayers = true;
+#endif
+
+class HelloTriangleApplication
 {
 public:
-    void Run() 
-    {
-        InitWindow();
-        InitVulkan();
-        Update();
-        Cleanup();
-    }
+	void Run()
+	{
+		InitWindow();
+		InitVulkan();
+		Update();
+		Cleanup();
+	}
 
 private:
-    GLFWwindow* window;
-    VkInstance m_instance;
+	GLFWwindow* window;
+	VkInstance m_instance;
 
-    void InitWindow() 
-    {
-        glfwInit();
+	void InitWindow()
+	{
+		glfwInit();
 
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
-    }
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+	}
 
-    void InitVulkan() 
-    {
-        CreateInstance();
+	void InitVulkan()
+	{
+		CreateInstance();
 
-    }
+	}
 
-    void Update() 
-    {
-        while (!glfwWindowShouldClose(window))
-        {
-            glfwPollEvents();
-        }
-    }
+	void Update()
+	{
+		while (!glfwWindowShouldClose(window))
+		{
+			glfwPollEvents();
+		}
+	}
 
-    void Cleanup() 
-    {
-        glfwDestroyWindow(window);
+	void Cleanup()
+	{
+		vkDestroyInstance(m_instance, nullptr);
 
-        glfwTerminate();
-    }
+		glfwDestroyWindow(window);
 
-    void CreateInstance()
-    {
-        VkApplicationInfo appInfo{};
-        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-        appInfo.pApplicationName = "Hello Triangle";
-        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.pEngineName = "No Engine";
-        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.apiVersion = VK_API_VERSION_1_4;
+		glfwTerminate();
+	}
 
-        VkInstanceCreateInfo createInfo{};
-        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-        createInfo.pApplicationInfo = &appInfo;
+	void CreateInstance()
+	{
+		if (enableValidationLayers && !CheckValidationLayerSupport())
+		{
+			throw std::runtime_error("Validation layers requested, but not available");
+		}
 
-        uint32_t glfwExtensionCount = 0;
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_4;
 
-        const char** glfwExtensions;
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
 
-        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+		uint32_t glfwExtensionCount = 0;
 
-        std::vector<const char*> requiredGlfwExtensions;
+		const char** glfwExtensions;
 
-        for (uint32_t i = 0; i < glfwExtensionCount; i++)
-        {
-            requiredGlfwExtensions.emplace_back(glfwExtensions[i]);
-        }
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
-        requiredGlfwExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+		std::vector<const char*> requiredGlfwExtensions;
 
-        ValidateExtensionAvailability(requiredGlfwExtensions);
+		for (uint32_t i = 0; i < glfwExtensionCount; i++)
+		{
+			requiredGlfwExtensions.emplace_back(glfwExtensions[i]);
+		}
 
-        createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+		requiredGlfwExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
 
-        createInfo.enabledExtensionCount = static_cast<uint32_t>(requiredGlfwExtensions.size());
-        createInfo.ppEnabledExtensionNames = requiredGlfwExtensions.data();
+		ValidateExtensionAvailability(requiredGlfwExtensions);
 
-        createInfo.enabledLayerCount = 0;
+		createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
 
-        if (vkCreateInstance(&createInfo, nullptr, &m_instance) != VK_SUCCESS)
-        {
-            throw std::runtime_error("failed to create instance");
-        }
-    }
+		createInfo.enabledExtensionCount = static_cast<uint32_t>(requiredGlfwExtensions.size());
+		createInfo.ppEnabledExtensionNames = requiredGlfwExtensions.data();
 
-    void ValidateExtensionAvailability(std::vector<const char*>& inputExtensions)
-    {
-        uint32_t extensionCount = 0;
-        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+		createInfo.enabledLayerCount = 0;
 
-        std::vector<VkExtensionProperties> extensions(extensionCount);
+		if (vkCreateInstance(&createInfo, nullptr, &m_instance) != VK_SUCCESS)
+		{
+			throw std::runtime_error("failed to create instance");
+		}
+	}
 
-        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+	void ValidateExtensionAvailability(std::vector<const char*>& inputExtensions)
+	{
+		assert((inputExtensions.size() > 0) && "input extension array is empty");
 
-        for (const auto& input : inputExtensions)
-        {
-            bool isAvailable = false;
-            for (const auto& extension : extensions)
-            {
-                if (std::string(input) == std::string(extension.extensionName)) isAvailable = true;
-            }
+		uint32_t extensionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
-            if (!isAvailable)
-            {
-                throw std::runtime_error("extension is not available");
-            }
-        }
-    }
+		std::vector<VkExtensionProperties> extensions(extensionCount);
+
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+		for (const auto& input : inputExtensions)
+		{
+			bool isAvailable = false;
+			for (const auto& extension : extensions)
+			{
+				if (strcmp(input, extension.extensionName) == 0)
+				{
+					isAvailable = true;
+					break;
+				}
+			}
+
+			if (!isAvailable)
+			{
+				throw std::runtime_error("extension is not available");
+			}
+		}
+	}
+
+	bool CheckValidationLayerSupport()
+	{
+		uint32_t layerCount;
+		vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+		std::vector<VkLayerProperties> availableLayers(layerCount);
+		vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+		for (const char* layerName : validationLayers)
+		{
+			bool layerFound = false;
+			for (const auto& layerProperties : availableLayers)
+			{
+				if (strcmp(layerName, layerProperties.layerName) == 0)
+				{
+					layerFound = true;
+					break;
+				}
+			}
+
+			if (!layerFound)
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
 };
 
 int main() {
-    HelloTriangleApplication app;
+	HelloTriangleApplication app;
 
-    try 
-    {
-        app.Run();
-    }
-    catch (const std::exception& e) 
-    {
-        std::cerr << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
+	try
+	{
+		app.Run();
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }

67480458048dfd3cf2baa3dc9b030c0f8d6eee75
Luc <lucmomber@outlook.com>
2025-05-08 18:30:32 +0200

    Added a DescriptorBuilder class.

---
+++ Engine/include/vkDescriptorBuilder.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "vkCommon.h"
+
+#include "vkDescriptorAllocator.h"
+
+class DescriptorLayoutCache;
+class DescriptorBuilder
+{
+public:
+	static DescriptorBuilder Begin(DescriptorLayoutCache* layoutCache, DescriptorAllocator* allocator);
+
+	DescriptorBuilder& BindBuffer(uint32_t binding, VkDescriptorBufferInfo* bufferInfo, VkDescriptorType type, VkShaderStageFlags stageFlags);
+	DescriptorBuilder& BindImage(uint32_t binding, VkDescriptorImageInfo* imageInfo, VkDescriptorType type, VkShaderStageFlags stageFlags);
+
+	bool Build(VkDescriptorSet& set, VkDescriptorSetLayout& layout);
+	bool Build(VkDescriptorSet& set);
+
+private:
+	DescriptorBuilder();
+	
+	std::vector<VkWriteDescriptorSet> m_writes;
+	std::vector<VkDescriptorSetLayoutBinding> m_bindings;
+
+	DescriptorLayoutCache* m_cache;
+	DescriptorAllocator* m_alloc;
+};
---
+++ Engine/source/vkDescriptorBuilder.cpp
@@ -0,0 +1,119 @@
+#include "vkDescriptionBuilder.h"
+
+#include "vkDescriptorLayoutCache.h"
+
+#include "engine.h"
+#include "vkDevice.h"
+
+DescriptorBuilder DescriptorBuilder::Begin(DescriptorLayoutCache* layoutCache, DescriptorAllocator* allocator)
+{
+	DescriptorBuilder builder;
+
+	builder.m_cache = layoutCache;
+	builder.m_alloc = allocator;
+
+	return builder;
+}
+
+DescriptorBuilder& DescriptorBuilder::BindBuffer(uint32_t binding, VkDescriptorBufferInfo* bufferInfo, VkDescriptorType type, VkShaderStageFlags stageFlags)
+{
+	VkDescriptorSetLayoutBinding newBinding{};
+	newBinding.descriptorCount = 1;
+	newBinding.descriptorType = type;
+	newBinding.pImmutableSamplers = nullptr;
+	newBinding.stageFlags = stageFlags;
+	newBinding.binding = binding;
+
+	m_bindings.push_back(newBinding);
+
+	VkWriteDescriptorSet newWrite{};
+	newWrite.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+	newWrite.pNext = nullptr;
+	newWrite.descriptorCount = 1;
+	newWrite.descriptorType = type;
+	newWrite.pBufferInfo = bufferInfo;
+	newWrite.dstBinding = binding;
+
+	m_writes.push_back(newWrite);
+
+	return *this;
+}
+
+DescriptorBuilder& DescriptorBuilder::BindImage(uint32_t binding, VkDescriptorImageInfo* imageInfo, VkDescriptorType type, VkShaderStageFlags stageFlags)
+{
+	VkDescriptorSetLayoutBinding newBinding{};
+	newBinding.descriptorCount = 1;
+	newBinding.descriptorType = type;
+	newBinding.pImmutableSamplers = nullptr;
+	newBinding.stageFlags = stageFlags;
+	newBinding.binding = binding;
+
+	m_bindings.push_back(newBinding);
+
+	VkWriteDescriptorSet newWrite{};
+	newWrite.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+	newWrite.pNext = nullptr;
+	newWrite.descriptorCount = 1;
+	newWrite.descriptorType = type;
+	newWrite.pBufferInfo = nullptr;
+	newWrite.pImageInfo = imageInfo;
+	newWrite.dstBinding = binding;
+
+	m_writes.push_back(newWrite);
+
+	return *this;
+}
+
+bool DescriptorBuilder::Build(VkDescriptorSet& set, VkDescriptorSetLayout& layout)
+{
+	VkDescriptorSetLayoutCreateInfo layoutInfo{};
+	layoutInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+	layoutInfo.pNext = nullptr;
+
+	layoutInfo.pBindings = m_bindings.data();
+	layoutInfo.bindingCount = static_cast<uint32_t>(m_bindings.size());
+
+	layout = m_cache->CreateDescriptorLayout(&layoutInfo);
+
+	bool success = m_alloc->Allocate(&set, layout);
+	if (!success)
+	{ 
+		return false; 
+	}
+
+	for (VkWriteDescriptorSet& w : m_writes)
+	{
+		w.dstSet = set;
+	}
+
+	vkUpdateDescriptorSets(Core::engine.GetDevice().GetVkDevice(), static_cast<uint32_t>(m_writes.size()), m_writes.data(), 0, nullptr);
+
+	return true;
+}
+
+bool DescriptorBuilder::Build(VkDescriptorSet& set)
+{
+	VkDescriptorSetLayoutCreateInfo layoutInfo{};
+	layoutInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+	layoutInfo.pNext = nullptr;
+
+	layoutInfo.pBindings = m_bindings.data();
+	layoutInfo.bindingCount = static_cast<uint32_t>(m_bindings.size());
+
+	VkDescriptorSetLayout layout = m_cache->CreateDescriptorLayout(&layoutInfo);
+
+	bool success = m_alloc->Allocate(&set, layout);
+	if (!success)
+	{
+		return false;
+	}
+
+	for (VkWriteDescriptorSet& w : m_writes)
+	{
+		w.dstSet = set;
+	}
+
+	vkUpdateDescriptorSets(Core::engine.GetDevice().GetVkDevice(), static_cast<uint32_t>(m_writes.size()), m_writes.data(), 0, nullptr);
+
+	return true;
+}
